350c3434058f9d49d688f3d7c737cdb77d5116d7
vishaga <com.gauravvishal@gmail.com>
2024-01-25 01:33:07 +0530

    ConsumerTest added

--- src/test/java/com/vishaga/java/util/ConsumerTest.java
+++ src/test/java/com/vishaga/java/util/ConsumerTest.java
@@ -1,4 +1,23 @@
 package com.vishaga.java.util;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ConsumerTest {
+
+    @Test
+    @DisplayName("Custom Consumer test")
+    public void consumerTest_1(){
+        Consumer<String> consumer1 = str -> {
+            System.out.println("DEBUG: " + str);
+            System.out.println("INFO: " + str);
+        };
+        Consumer<String> consumer2 = consumer1.andThen(s -> System.out.println("ERROR: "+s));
+
+        consumer1.accept("Gaurav Vishal");
+        System.out.println("========================== ");
+        consumer2.accept("Vivek Vishal");
+    }
 }
