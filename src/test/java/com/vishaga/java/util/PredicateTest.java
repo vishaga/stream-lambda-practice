5b7b9757958c8d27a8e54328d0059df487f6d49d
vishaga <com.gauravvishal@gmail.com>
2024-01-25 01:14:16 +0530

    PredicateTest test cases  added

--- src/test/java/com/vishaga/java/util/PredicateTest.java
+++ src/test/java/com/vishaga/java/util/PredicateTest.java
@@ -1,4 +1,19 @@
 package com.vishaga.java.util;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PredicateTest {
+
+    @Test
+    @DisplayName("Custom Predicate test")
+    public void predicateTest_1(){
+        Predicate<Integer> predicate1 = x -> x % 2 == 0;
+
+        assertThat(predicate1.test(5)).isFalse();
+        assertThat(predicate1.test(16)).isTrue();
+        assertThat(predicate1.negate().test(16)).isFalse();
+    }
 }
