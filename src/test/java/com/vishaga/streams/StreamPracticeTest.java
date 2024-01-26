916409d0c692495244838c433a41521175e658e9
vishaga <com.gauravvishal@gmail.com>
2024-01-27 01:59:20 +0530

    Stream practice test cases

---
+++ src/test/java/com/vishaga/streams/StreamPracticeTest.java
@@ -0,0 +1,175 @@
+package com.vishaga.streams;
+
+import com.vishaga.utils.DataLoaderUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StreamPracticeTest {
+
+    @BeforeAll
+    public static void setUp(){
+        //COUNTRIES = DataLoaderUtils.loadCountries();
+    }
+
+    @Test
+    @DisplayName("Given a list of integers, find the sum of all even numbers.")
+    public void practiceTest_1(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        int sum = numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .mapToInt(number -> number)
+                .sum();
+
+        assertThat(sum).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("Given a list of strings, convert each string to uppercase and collect the result into a new list.")
+    public void practiceTest_2(){
+        List<String> words = Arrays.asList("apple", "banana", "orange", "grape");
+
+        List<String> upperCaseWords = words.stream()
+                .map(String::toUpperCase)
+                .toList();
+
+        assertThat(upperCaseWords).contains("APPLE","BANANA", "ORANGE", "GRAPE");
+    }
+
+    @Test
+    @DisplayName("Given a list of words, count the number of words that start with the letter 'a'.")
+    public void practiceTest_3(){
+        List<String> words = Arrays.asList("apple", "banana", "avocado", "grape", "apricot");
+
+        long wordsStartingWithA = words.stream()
+                .filter(word -> word.startsWith("a"))
+                .count();
+
+        assertThat(wordsStartingWithA).isEqualTo(3L);
+    }
+
+    @Test
+    @DisplayName("Given a list of employees with their salaries, find the average salary.")
+    public void practiceTest_4(){
+        record Employee(String name, int salary){};
+
+        List<Employee> employees = Arrays.asList(
+                new Employee("John", 50000),
+                new Employee("Alice", 60000),
+                new Employee("Bob", 75000),
+                new Employee("Charlie", 80000)
+        );
+
+        double averageSalary = employees.stream()
+                .mapToInt(employee -> employee.salary)
+                .summaryStatistics().getAverage();
+
+        assertThat(averageSalary).isEqualTo(66_250L);
+    }
+
+    @Test
+    @DisplayName("Given a list of numbers, filter out the even numbers, square each remaining number, and then find the sum.")
+    public void practiceTest_5(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        double squareOfOddNumbers = numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .mapToInt(number -> number * number)
+                .sum();
+
+        assertThat(squareOfOddNumbers).isEqualTo(165L);
+    }
+
+    @Test
+    @DisplayName("Given a list of books, group them by genre and count the number of books in each genre.")
+    public void practiceTest_6(){
+        record Book(String genre, String name){};
+
+        List<Book> books = Arrays.asList(
+                new Book("Sci-Fi", "Dune"),
+                new Book("Fantasy", "Harry Potter"),
+                new Book("Sci-Fi", "Foundation"),
+                new Book("Mystery", "Sherlock Holmes"),
+                new Book("Fantasy", "Lord of the Rings"),
+                new Book("Mystery", "Murder on the Orient Express"),
+                new Book("Programming", "Head First Java")
+        );
+        Map<String, Long> booksByGenre = books.stream()
+                .collect(Collectors.groupingBy(Book::genre, Collectors.counting()));
+
+        assertThat(booksByGenre).containsAllEntriesOf(
+                Map.ofEntries(
+                        entry("Sci-Fi",2L),
+                        entry("Fantasy",2L),
+                        entry("Mystery",2L),
+                        entry("Programming",1L)
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("Given a list of strings, filter out the strings with length less than 4 " +
+            "and join the remaining strings into a single string, separated by commas.")
+    public void practiceTest_7(){
+        List<String> words = Arrays.asList("apple", "banana", "kiwi", "grape", "peach");
+
+
+        String newString = words.stream()
+                .filter(word -> word.length() > 4)
+                .collect(Collectors.joining(","));
+
+        assertThat(newString).isEqualTo("apple,banana,grape,peach");
+    }
+
+    @Test
+    @DisplayName("Given a list of words, create a list of all unique characters present in those words.")
+    public void practiceTest_8(){
+        List<String> words = Arrays.asList("hello", "world", "java", "stream");
+
+        Set<String> uniqueCharacters = words.stream()
+                .flatMap(word -> Arrays.stream(word.split("")))
+                .collect(Collectors.toSet());
+
+        assertThat(uniqueCharacters).contains("h","e","l","o","w","r","d","j","a","v","s","t","m");
+    }
+
+    @Test
+    @DisplayName("Given a list of numbers, partition them into two groups - even and odd.")
+    public void practiceTest_9(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        Map<Boolean, List<Integer>> evenOddNumbers = numbers.stream()
+                .collect(Collectors.partitioningBy(num -> num % 2 == 0));
+
+        assertThat(evenOddNumbers).containsAllEntriesOf(
+                Map.ofEntries(
+                        entry(true,List.of(2,4,6,8,10)),
+                        entry(false,List.of(1,3,5,7,9))
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("Given a list of integers, find the maximum value using the reduce operation. " +
+            "Return an Optional to handle the case where the list is empty.")
+    public void practiceTest_10(){
+        List<Integer> numbers = Arrays.asList(15, 8, 20, 12, 7, 18);
+
+        Optional<Integer> maxNumber = numbers.stream()
+                .reduce(Math::max);
+
+        assertThat(maxNumber.get()).isEqualTo(20);
+
+        Optional<Integer> empty = Stream.<Integer>of()
+                .reduce(Math::max);
+
+        assertThat(empty).isEqualTo(Optional.empty());
+    }
+}
