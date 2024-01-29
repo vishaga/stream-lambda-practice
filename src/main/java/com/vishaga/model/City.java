e222c4cfa2e12e88b908d97da98f733f32c90df4
vishaga <com.gauravvishal@gmail.com>
2024-01-29 19:30:30 +0530

    toMap test cases added

--- src/main/java/com/vishaga/model/City.java
+++ src/main/java/com/vishaga/model/City.java
@@ -3,12 +3,12 @@ package com.vishaga.model;
 public record City(int rank, String name, String country, int population, int metroPopulation) {
 
     public static City from(String line){
-        String attributes[] = line.split("\t");
+        String[] attributes = line.split("\t");
         int rank = Integer.parseInt(attributes[0].trim());
-        String name = attributes[0].trim();
-        String country = attributes[0].trim();
-        int population = Integer.parseInt(attributes[0].trim());
-        int metroPopulation = Integer.parseInt(attributes[0].trim());
+        String name = attributes[1].trim();
+        String country = attributes[2].trim();
+        int population = Integer.parseInt(attributes[3].trim());
+        int metroPopulation = Integer.parseInt(attributes[4].trim());
         return new City(rank, name, country, population, metroPopulation);
     }
 }
--- src/test/java/com/vishaga/streams/PlayWithCollectorsToMapTest.java
+++ src/test/java/com/vishaga/streams/PlayWithCollectorsToMapTest.java
@@ -1,2 +1,75 @@
-package com.vishaga.streams;public class PlayWithCollectorsToMapTest {
+package com.vishaga.streams;
+
+import com.vishaga.model.*;
+import com.vishaga.utils.DataLoaderUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayWithCollectorsToMapTest {
+
+    static List<City> CITIES;
+
+    @BeforeAll
+    public static void setUp(){
+        CITIES = DataLoaderUtils.loadCities();
+    }
+
+    @Test
+    @DisplayName("Using toMap(KeyMapper, ValueMapper)")
+    public void test_1(){
+        Map<Integer, Integer> populationByRank = CITIES.stream()
+                .collect(
+                        Collectors.toMap(
+                                City::rank,
+                                City::population
+                        )
+                );
+        assertThat(populationByRank).containsAllEntriesOf(
+                Map.ofEntries(
+                        Map.entry(1,24153000),
+                        Map.entry(2,18590000),
+                        Map.entry(3,18000000),
+                        Map.entry(4,14657000),
+                        Map.entry(297,595000),
+                        Map.entry(298,578000),
+                        Map.entry(299,433000),
+                        Map.entry(300,361000)
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("Using toMap(KeyMapper, ValueMapper, MergeFunction)")
+    public void test_2(){
+        Map<String, Integer> populationByCountry = CITIES.stream()
+                .collect(
+                        Collectors.toMap(
+                                City::country,
+                                City::population,
+                                //(p1, p2) -> p1 + p2
+                                Integer::sum
+                        )
+                );
+
+        assertThat(populationByCountry).containsAllEntriesOf(
+                Map.ofEntries(
+                        Map.entry("Cameroon",4982000),
+                        Map.entry("Angola",2108000),
+                        Map.entry("Cambodia",1502000),
+                        Map.entry("India",96767000),
+                        Map.entry("Germany",6908000),
+                        Map.entry("China",253364300)
+
+                )
+        );
+    }
 }
