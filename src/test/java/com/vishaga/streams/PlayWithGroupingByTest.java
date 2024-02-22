b192d1aab2e21d39eafb941be1046a14c03fcb5c
vishaga <com.gauravvishal@gmail.com>
2024-02-22 18:02:00 +0530

    added more test cases

--- src/test/java/com/vishaga/streams/PlayWithGroupingByTest.java
+++ src/test/java/com/vishaga/streams/PlayWithGroupingByTest.java
@@ -9,13 +9,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 public class PlayWithGroupingByTest {
 
@@ -46,6 +44,9 @@ public class PlayWithGroupingByTest {
                 .collect(
                         Collectors.groupingBy(
                                 Country::region));
+
+
+
         Set<Region> regions = countriesByRegion.keySet();
         assertThat(regions.size()).isEqualTo(6);
     }
@@ -65,4 +66,36 @@ public class PlayWithGroupingByTest {
         assertThat(regions).containsExactly("AFRICA", "AMERICAS", "ASIA", "EUROPE", "NA", "OCEANIA");
     }
 
+    @Test
+    @DisplayName("Number of Countries by region.")
+    public void groupingBy_3(){
+        Map<String, Long> numberOfCountriesByRegion = COUNTRIES.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Country::subRegion,
+                                Collectors.counting()));
+
+        assertThat(numberOfCountriesByRegion.size()).isEqualTo(18);
+
+        assertThat(numberOfCountriesByRegion).containsExactly(
+                entry("Northern Africa", 7L),
+                entry("Northern Europe", 16L),
+                entry("Eastern Europe", 10L),
+                entry("Australia and New Zealand", 6L),
+                entry("Southern Asia", 9L),
+                entry("Central Asia", 5L),
+                entry("Sub-Saharan Africa", 53L),
+                entry("Western Europe", 9L),
+                entry("Northern America", 5L),
+                entry("NA", 1L),
+                entry("Western Asia", 18L),
+                entry("Polynesia", 10L),
+                entry("Melanesia", 5L),
+                entry("South-eastern Asia", 11L),
+                entry("Latin America and the Caribbean", 52L),
+                entry("Southern Europe", 16L),
+                entry("Micronesia", 8L),
+                entry("Eastern Asia", 8L)
+        );
+    }
 }
